96e4b6cc1f0d37dd902fbeb89070092958d09886
Anissa <anissa.dahabi@gmail.com>
2026-03-10 11:35:52 +0100

    Delete vue.py

--- vue.py
+++
@@ -1,7 +0,0 @@
-def afficher_menu():
-    print("\n --- Menu ---")
-    print("1. Afficher les mots")
-    print("2. Ajouter un mot")
-    print("3. Supprimer un mot")
-    print("4. Modifier un mot")
-    print("0. Quitter")
